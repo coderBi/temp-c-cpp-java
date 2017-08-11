820be7f45428fbd3834e942b2c19c042a2bd64f3
coderbi <770168408@qq.com>
2017-08-11 19:54:32 +0800

    Revert "整型与指针之间的转换，在"
    This reverts commit 96c518dccc8b293bacc1b917288331a481228290.

--- int和指针的直接转换/C语言中int与指针的直接转换.c
+++
@@ -1,9 +0,0 @@
-int main(){
-	int i = 0;
-	int j = &i;  //出现了一个警告，说是用指针初始化一个int但是没有进行转换(cast)
-	int m = (int)&i; //正常
-
-	int *p = 111; //也是一个警告，说是没有经过cast直接用int给指针赋值
-	int *p1 = (int*)111;
-	return 0;
-}
